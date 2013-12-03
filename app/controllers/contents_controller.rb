4e2e0180f676e7e1abd661d4e5104a0c364b25de
Silvio Vaz <hello@sevenvisualsigns.com>
2013-12-03 12:30:10 +0000

    First stage of the project, Post model and controller and contents controller

---
+++ app/controllers/contents_controller.rb
@@ -0,0 +1,22 @@
+class ContentsController < ApplicationController
+  def investment
+  end
+
+  def partner
+  end
+
+  def companies
+  end
+
+  def contact
+  end
+
+  def sitemap
+  end
+
+  def terms
+  end
+
+  def privacy
+  end
+end
