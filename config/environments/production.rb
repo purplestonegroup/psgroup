caa60f22fbcdb1b7412bed378f30dddaa9a7e053
Silvio Vaz <hello@sevenvisualsigns.com>
2013-12-06 11:07:04 +0000

    5 change of the s3 behavior

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,9 +83,9 @@ Group::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['PURPLESTONE-IMG'],
-      :access_key_id => ENV['AKIAIBZS3A73ARFOXFBA'],
-      :secret_access_key => ENV['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']
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 end
