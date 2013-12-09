31bf286c43a6933ac2743febaa2fcb777c3565ab
Silvio Vaz <hello@sevenvisualsigns.com>
2013-12-09 11:25:13 +0000

    changes in the cantact_mail.rb files with the gmail account

--- app/mailers/contact_mail.rb
+++ app/mailers/contact_mail.rb
@@ -2,9 +2,9 @@ class ContactMail < ActionMailer::Base
   
   attr_accessor :name, :email, :phone, :subject, :comments
 
-  default to: 'directorgeneral@pstonemedia.com', from: 'noreply@purplestonegroup.com'
+  default to: 'purplestonegroup.com@gmail.com', from: 'purplestonegroup.com@gmail.com'
 
-  headers = {'Return-Path' => 'noreply@purplestonegroup.com'}
+  headers = {'Return-Path' => 'purplestonegroup.com@gmail.com'}
 
   def contact_email(contact)
     
@@ -15,8 +15,8 @@ class ContactMail < ActionMailer::Base
     mail(
     	to: @contact.email,
     	subject: 'Message from the contact form of Purple Stone Group'
-    	from: "PurpleStoneGroup <noreply@purplestonegroup.com>",
-      	return_path: "noreply@purplestonegroup.com",
+    	from: "PurpleStoneGroup <purplestonegroup.com@gmail.com>",
+      	return_path: "purplestonegroup.com@gmail.com",
       	date: Time.now,
       	content_type: "text/html"
     )
